c37853180beb17b607bfed5abad01538f1924b26
Nguyen Xuan Cong <uspro@USPros-MacBook-Pro.local>
2023-07-07 22:09:29 +0700

    remove react, add vue, locales

---
+++ src/popup/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+    import logo from 'url:../images/icon256.png'
+</script>
+<template>
+    <img :src="logo" alt="logo"/>
+    <div>Hello world</div>
+</template>
---
+++ src/shared/config.js
@@ -0,0 +1,2 @@
+globalThis.__VUE_OPTIONS_API__ = false;
+globalThis.__VUE_PROD_DEVTOOLS__ = false;
